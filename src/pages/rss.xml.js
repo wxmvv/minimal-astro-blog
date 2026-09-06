ba967846550ae786898d00c52b7b79e7495a3d02
wxm <w05172075@outlook.com>
2026-09-06 09:00:14 +0800

    feat: draft适配

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss';
 import { siteMetadata } from '@data/config/site';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
   return rss({
     title: siteMetadata.title,
     description: siteMetadata.description,
