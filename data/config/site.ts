3d7668b47faba33606f30539abb95fa59ab65bd1
wxm <w05172075@outlook.com>
2026-09-14 10:46:13 +0800

    feat: 对接umamiAnalytics

--- data/config/site.ts
+++ data/config/site.ts
@@ -22,25 +22,10 @@ export const siteMetadata = {
   github: 'https://github.com/wxmvv',
   rssPath: '/rss.xml',
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    // Loaded in production only. Set umamiWebsiteId to '' to disable tracking.
     // umamiAnalytics: {
-    // We use an env variable for this site to avoid other users cloning our analytics ID
-    // umamiWebsiteId: process.env.NEXT_UMAMI_ID // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // You may also need to overwrite the script if you're storing data in the US - ex:
-    // src: 'https://us.umami.is/script.js'
-    // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-    // }
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    //   umamiWebsiteId: '',
+    //   src: '',
     // },
   },
   comments: {
